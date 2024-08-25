d20b133f9ca507e134f9f8e8f269ac0a08b77d36
DW Wheeler <dawn.wheeler@gmail.com>
2024-08-24 22:19:43 -0700

    working on laughing

--- oled_test/oled_test/oled_test.ino
+++ oled_test/oled_test/oled_test.ino
@@ -13,17 +13,17 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define LOGO_HEIGHT   16
 #define LOGO_WIDTH    16
 
-#define EYE_RADIUS          18
-#define EYE_X_INSET         5    // side of screen to outside edge of eye
-#define EYE_Y_INSET         5    // top of screen to top edge of eye
-#define PUPIL_RADIUS        16
+#define EYE_RADIUS       18
+#define EYE_X_INSET      5    // side of screen to outside edge of eye
+#define EYE_Y_INSET      5    // top of screen to top edge of eye
+#define PUPIL_RADIUS     16
 #define HILITE_RADIUS    2
 #define HILITE_OFFSET_X  6
 #define HILITE_OFFSET_Y  6
 
-#define MOUTH_RADIUS 15
-#define MOUTH_OFFSET 6 // Bottom of screen to bottom of smile
-#define MOUTH_HEIGHT 4 // Bottom of smile to top of smile
+#define MOUTH_RADIUS 15  // Radius of circle used to draw mouth
+#define MOUTH_OFFSET 6   // Bottom of screen to bottom of smile
+#define MOUTH_HEIGHT 4   // Bottom of smile to top of smile
 
 // Using ESP32, connect to OLED:
 // 3V3 - VCC
@@ -35,9 +35,9 @@ void setup() {
   Serial.begin(115200);
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for (;;); // Don't proceed, loop forever
   }
 
   // Show initial display buffer contents on the screen --
@@ -47,39 +47,41 @@ void setup() {
 
   // Clear the buffer
   display.clearDisplay();
- 
+
 }
 
 void loop() {
 
-  drawFace(0,0, 1000);
- // drawFace(-1,0, 2000);  
-  blinkEyes(-1,0);
-  
-//  drawFace(0,0, 2000);
-//
-//  drawFace(-1,0, 1000);
-//  drawFace(0,0, 2000);
-//  drawFace(2,0, 2000);
-//  drawFace(2,-2, 1000);
-//
-//  blinkEyes(2,-2);
-//
-//  drawFace(0,0, 2000);
-//  drawFace(1,0, 2000);
-//  
-//  blinkEyes(1,0);
-//
-//  drawFace(-2,0,2000);
-//  drawFace(0,0, 2000);
-//  drawFace(0,-2, 1000);
-//  drawFace(0,0, 2000);
-//  drawFace(-2,0, 1000);
-// 
-//  blinkEyes(-1,0);
-//
-//  drawFace(0,0, 2000);
-//  drawFace(-2,0, 3000);
+  drawFace(0, 0, 1000);
+  doLaugh();
+
+  // drawFace(-1,0, 2000);
+  //  blinkEyes(-1,0);
+
+  //  drawFace(0,0, 2000);
+  //
+  //  drawFace(-1,0, 1000);
+  //  drawFace(0,0, 2000);
+  //  drawFace(2,0, 2000);
+  //  drawFace(2,-2, 1000);
+  //
+  //  blinkEyes(2,-2);
+  //
+  //  drawFace(0,0, 2000);
+  //  drawFace(1,0, 2000);
+  //
+  //  blinkEyes(1,0);
+  //
+  //  drawFace(-2,0,2000);
+  //  drawFace(0,0, 2000);
+  //  drawFace(0,-2, 1000);
+  //  drawFace(0,0, 2000);
+  //  drawFace(-2,0, 1000);
+  //
+  //  blinkEyes(-1,0);
+  //
+  //  drawFace(0,0, 2000);
+  //  drawFace(-2,0, 3000);
 }
 
 void drawFace(int left, int up, int hold) {
@@ -93,12 +95,33 @@ void drawFace(int left, int up, int hold) {
 }
 
 void blinkEyes(int left, int up) {
-  for(int i=0; i<=100; i+=33) { //33 was good
-    drawBlinkStep(left, up, i, 10); // 10 was good
-  }
-  for(int i=100; i>0; i-=33) {
+  for (int i = 0; i <= 100; i += 33) {
     drawBlinkStep(left, up, i, 10);
   }
+  for (int i = 100; i > 0; i -= 33) {
+    drawBlinkStep(left, up, i, 10);
+  }
+}
+
+void doLaugh() {
+  drawLaughStep(0, -5, 500);
+ 
+  for (int i = 0; i < 5; i++) {
+    drawLaughStep(0, -3, 200);
+    drawLaughStep(0, -5, 200);
+  }
+}
+
+void drawLaughStep(int left, int up, int hold) {
+  int leftEyeCenterX = calcLeftEyeCenterX(left, up);
+  int rightEyeCenterX = calcRightEyeCenterX(left, up);
+  int eyeCenterY = calcEyeCenterY(up);
+
+  drawLaughingMouth();
+  drawLaughEyes(left, up, leftEyeCenterX, rightEyeCenterX, eyeCenterY);
+
+  display.display();
+  delay(hold);
 }
 
 void drawBlinkStep(int left, int up, int percentClosed, int hold) {
@@ -111,7 +134,7 @@ void drawBlinkStep(int left, int up, int percentClosed, int hold) {
 
   int lidCenterY = map(percentClosed, 0, 100,
                        calcLidCenterOpen(up, eyeCenterY),
-                       calcLidCenterClosed(up, eyeCenterY)); 
+                       calcLidCenterClosed(up, eyeCenterY));
 
   // draw "lid" (doubled line)
   display.fillCircle(leftEyeCenterX,
@@ -121,88 +144,55 @@ void drawBlinkStep(int left, int up, int percentClosed, int hold) {
 
   display.fillCircle(leftEyeCenterX,
                      lidCenterY,
-                     EYE_RADIUS-2,
+                     EYE_RADIUS - 2,
                      SSD1306_BLACK);
-                     
+
   display.fillCircle(rightEyeCenterX,
                      lidCenterY,
                      EYE_RADIUS,
                      SSD1306_WHITE);
-                     
+
   display.fillCircle(rightEyeCenterX,
                      lidCenterY,
-                     EYE_RADIUS-2,
+                     EYE_RADIUS - 2,
                      SSD1306_BLACK);
-                     
-  // "erase" top part           
-  display.fillRect(0,0,
-                   display.width(), (lidCenterY+eyeCenterY)/2,
+
+  // "erase" top part
+  display.fillRect(0, 0,
+                   display.width(), (eyeCenterY + lidCenterY) / 2,
                    SSD1306_BLACK);
   display.display();
   delay(hold);
 }
 
-void laughEyes() {
-  int leftEyeCenterX = calcLeftEyeCenterX(left, up);
-  int rightEyeCenterX = calcRightEyeCenterX(left, up);
-  int eyeCenterY = calcEyeCenterY(up);
-
-  drawSmileMouth();
-  drawEyes(left, up, leftEyeCenterX, rightEyeCenterX, eyeCenterY);
-
-  int lidCenterY = map(percentClosed, 0, 100,
-                       calcLidCenterOpen(up, eyeCenterY),
-                       calcLidCenterClosed(up, eyeCenterY)); 
-
-  // draw "lid" (doubled line)
-  display.fillCircle(leftEyeCenterX,
-                     lidCenterY,
-                     EYE_RADIUS,
-                     SSD1306_WHITE);
-
-  display.fillCircle(leftEyeCenterX,
-                     lidCenterY,
-                     EYE_RADIUS-2,
-                     SSD1306_BLACK);
-                     
-  display.fillCircle(rightEyeCenterX,
-                     lidCenterY,
-                     EYE_RADIUS,
-                     SSD1306_WHITE);
-                     
-  display.fillCircle(rightEyeCenterX,
-                     lidCenterY,
-                     EYE_RADIUS-2,
-                     SSD1306_BLACK);
-                     
-  // "erase" top part           
-  display.fillRect(0,0,
-                   display.width(), (lidCenterY+eyeCenterY)/2,
-                   SSD1306_BLACK);
-  display.display();
-  delay(hold);
-}
-
-void drawLaughEyes() {
-  int leftEyeCenterX = calcLeftEyeCenterX(0, 0);
-  int rightEyeCenterX = calcRightEyeCenterX(0, 0);
-  int eyeCenterY = calcEyeCenterY(0);
-
-  
-}
-
+// Standard mouth
 void drawSmileMouth() {
   int screenCenterX = display.width() / 2;
   int mouthCenterY = display.height() - MOUTH_OFFSET - MOUTH_RADIUS;
 
   // doubled line
   display.drawCircle(screenCenterX, mouthCenterY, MOUTH_RADIUS, SSD1306_WHITE);
-  display.drawCircle(screenCenterX, mouthCenterY, MOUTH_RADIUS-1, SSD1306_WHITE);
+  display.drawCircle(screenCenterX, mouthCenterY, MOUTH_RADIUS - 1, SSD1306_WHITE);
 
   // erase top part
-  display.fillRect(0,0,
-                   display.width(), calcMouthBoxY(),
+  display.fillRect(0, 0,
+                   display.width(), calcTopOfMouth(),
                    SSD1306_BLACK);
+}
+
+// Standard mouth
+void drawLaughingMouth() {
+  int screenCenterX = display.width() / 2;
+  int mouthCenterY = display.height() - MOUTH_OFFSET - MOUTH_RADIUS;
+
+  // doubled line
+  display.drawCircle(screenCenterX, mouthCenterY, MOUTH_RADIUS, SSD1306_WHITE);
+  display.drawCircle(screenCenterX, mouthCenterY, MOUTH_RADIUS - 1, SSD1306_WHITE);
+
+  // erase top part
+  display.fillRect(0, 0,
+                   display.width(), calcTopOfMouth() - MOUTH_HEIGHT,
+                   SSD1306_INVERSE);
 }
 
 void drawOooMouth() {
@@ -211,28 +201,113 @@ void drawOooMouth() {
   int mouthCenterY = display.height() - MOUTH_OFFSET - oooRadius;
 
   display.drawCircle(screenCenterX, mouthCenterY, oooRadius, SSD1306_WHITE);
-  display.drawCircle(screenCenterX, mouthCenterY, oooRadius-1, SSD1306_WHITE);
+  display.drawCircle(screenCenterX, mouthCenterY, oooRadius - 1, SSD1306_WHITE);
 }
 
 /**
- * Draw "standard" eyes, shifted left and up pixels in those directions
- * (left and up can be negative)
- */
+   Draw "standard" eyes, shifted left and up pixels in those directions
+   (left and up can be negative)
+*/
 void drawEyes(int left, int up) {
   int leftEyeCenterX = calcLeftEyeCenterX(left, up);
   int rightEyeCenterX = calcRightEyeCenterX(left, up);
   int eyeCenterY = calcEyeCenterY(up);
 
+  // usually we want highlights
   drawEyes(left, up, leftEyeCenterX, rightEyeCenterX, eyeCenterY);
 }
 
+/**
+   Laugh eyes are wider
+*/
+void drawLaughEyes(int left, int up, int leftEyeCenterX, int rightEyeCenterX, int eyeCenterY) {
+  int squintWidening = 4;
+
+  // draw white
+  display.fillCircle(leftEyeCenterX,
+                     eyeCenterY,
+                     EYE_RADIUS + squintWidening,
+                     SSD1306_WHITE);
+
+  display.fillCircle(rightEyeCenterX,
+                     eyeCenterY,
+                     EYE_RADIUS + squintWidening,
+                     SSD1306_WHITE);
+
+  // draw pupil
+  display.fillCircle(leftEyeCenterX,
+                     eyeCenterY,
+                     PUPIL_RADIUS + squintWidening,
+                     SSD1306_INVERSE);
+
+  display.fillCircle(rightEyeCenterX,
+                     eyeCenterY,
+                     PUPIL_RADIUS + squintWidening,
+                     SSD1306_INVERSE);
+
+  int shrinkHighlight = 0;
+  int raiseHighlight = 6;
+
+  // draw highlights
+  display.fillCircle(calcLeftEyeHighlightX(left, up),
+                     calcEyeHighlightY(up) - raiseHighlight,
+                     HILITE_RADIUS - shrinkHighlight,
+                     SSD1306_INVERSE);
+
+  display.fillCircle(calcRightEyeHighlightX(left, up),
+                     calcEyeHighlightY(up) - raiseHighlight,
+                     HILITE_RADIUS - shrinkHighlight,
+                     SSD1306_INVERSE);
+
+                     
+  int percentSquint = 60;
+  // circle pushing up eye from below
+  int cheekCenterY = map(percentSquint, 100, 0,
+                         calcCheekCenterSquinting(up, eyeCenterY),
+                         calcCheekCenterNoSquinting(up, eyeCenterY));
+                         
+
+                      // deform eye
+  display.fillCircle(leftEyeCenterX,
+                     cheekCenterY,
+                     EYE_RADIUS,
+                     SSD1306_WHITE);
+
+  display.fillCircle(leftEyeCenterX,
+                     cheekCenterY,
+                     EYE_RADIUS - 2,
+                     SSD1306_BLACK);
+
+  display.fillCircle(rightEyeCenterX,
+                     cheekCenterY,
+                     EYE_RADIUS,
+                     SSD1306_WHITE);
+
+  display.fillCircle(rightEyeCenterX,
+                     cheekCenterY,
+                     EYE_RADIUS - 2,
+                     SSD1306_BLACK);
+
+  // "erase" bottom parts
+  display.fillRect(leftEyeCenterX - EYE_RADIUS, (eyeCenterY + cheekCenterY) / 2,
+                   2 * EYE_RADIUS + 2, 2 * EYE_RADIUS,
+                   SSD1306_BLACK);
+
+  display.fillRect(rightEyeCenterX - EYE_RADIUS, (eyeCenterY + cheekCenterY) / 2,
+                   2 * EYE_RADIUS + 2, 2 * EYE_RADIUS,
+                   SSD1306_BLACK);
+}
+
+/**
+   Standard circular eye with highlights
+*/
 void drawEyes(int left, int up, int leftEyeCenterX, int rightEyeCenterX, int eyeCenterY) {
   // draw white
   display.fillCircle(leftEyeCenterX,
                      eyeCenterY,
                      EYE_RADIUS,
                      SSD1306_WHITE);
-                     
+
   display.fillCircle(rightEyeCenterX,
                      eyeCenterY,
                      EYE_RADIUS,
@@ -243,13 +318,13 @@ void drawEyes(int left, int up, int leftEyeCenterX, int rightEyeCenterX, int eye
                      eyeCenterY,
                      PUPIL_RADIUS,
                      SSD1306_INVERSE);
-                     
+
   display.fillCircle(rightEyeCenterX,
                      eyeCenterY,
                      PUPIL_RADIUS,
                      SSD1306_INVERSE);
 
-  // draw Highlights
+  // draw highlights
   display.fillCircle(calcLeftEyeHighlightX(left, up),
                      calcEyeHighlightY(up),
                      HILITE_RADIUS,
@@ -261,8 +336,8 @@ void drawEyes(int left, int up, int leftEyeCenterX, int rightEyeCenterX, int eye
                      SSD1306_INVERSE);
 }
 
-int calcMouthBoxY() {
-  return display.height() - (MOUTH_OFFSET+MOUTH_HEIGHT);
+int calcTopOfMouth() {
+  return display.height() - (MOUTH_OFFSET + MOUTH_HEIGHT);
 }
 
 int calcLeftEyeCenterX(int left, int up) {
@@ -270,7 +345,7 @@ int calcLeftEyeCenterX(int left, int up) {
 }
 
 int calcRightEyeCenterX(int left, int up) {
-  return display.width() - EYE_X_INSET - EYE_RADIUS - left -1;
+  return display.width() - EYE_X_INSET - EYE_RADIUS - left - 1;
 }
 
 int calcEyeCenterY(int up) {
@@ -298,4 +373,12 @@ int calcLidCenterOpen(int up, int eyeCenterY) {
 
 int calcLidCenterClosed(int up, int eyeCenterY) {
   return eyeCenterY;
+}
+
+int calcCheekCenterSquinting(int up, int eyeCenterY) {
+  return eyeCenterY;
+}
+
+int calcCheekCenterNoSquinting(int up, int eyeCenterY) {
+  return eyeCenterY + 2 * EYE_RADIUS;
 }
